0c5dc021133a3827a03aeabb8e886862d281e16e
reyzin <devreyz137@gmail.com>
2024-03-08 21:42:27 -0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    colors: {
+      primary: "#5EE088",
+      secondary: "#87CEEB",
+      accent: "#FFD966",
+      dark_color: "#1A2736",
+      light_color: "#D5D8DE"
+    },
+    extend: {},
+  },
+  plugins: [],
+};
