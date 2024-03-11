e064539adff7970517f1b7cb0a8da5883e55e80d
Reyz <devreyz137@gmail.com>
2024-03-11 17:46:55 -0300

    Criado barra de navegação responsiva, adicionado estrutura de visualização de produto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    colors: {
-      primary: "#5EE088",
-      secondary: "#87CEEB",
-      accent: "#FFD966",
-      dark_color: "#1A2736",
-      light_color: "#D5D8DE"
-    },
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5EE088",
+        secondary: "#87CEEB",
+        accent: "#FFD966",
+        darkcolor: "#1A2736",
+        lightcolor: "#D5D8DE",
+        textcolor: colors.slate['800'],
+        menu: colors.gray['500'],
+        headerbg: colors.gray['500']
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
